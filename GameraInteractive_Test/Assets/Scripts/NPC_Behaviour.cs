ddfbfe1255840a6ed1f5aba16cc0b0eb6c792f21
FraDM89 <42539607+FraDM89@users.noreply.github.com>
2020-03-06 14:53:17 +0100

    6/03/20 - mattina
    Setting player movement
    Setting NPC behaviour

---
+++ GameraInteractive_Test/Assets/Scripts/NPC_Behaviour.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum npcType { villager, merchant, standardEnemy}
+
+ [RequireComponent(typeof(NavMeshAgent))]
+public class NPC_Behaviour : MonoBehaviour
+{
+    public npcType npcType;
+
+    public Transform[] waypoints;
+    public Transform raycastPoint;
+    public float timeOnWaypoint = 1f;
+    public float enemyFieldOfView;
+    public float rotSpeed = 0.5f;
+
+
+    private NavMeshAgent npcAgent;
+    private SphereCollider npcCollider;
+    private bool patroling;
+    private bool readyToInteract, playerInteract;
+    private bool searchPlayer;
+    private int currentWaypoint = 0;
+    private float startTimeOnWaypoint;
+    private float startAgentSpeed;
+    
+
+    void Awake()
+    {
+        npcAgent = GetComponent<NavMeshAgent>();
+        npcCollider = GetComponent<SphereCollider>();
+    }
+
+    void Start()
+    {
+        startTimeOnWaypoint = timeOnWaypoint;
+        startAgentSpeed = npcAgent.speed;
+        patroling = true;
+    }
+    
+    void Update()
+    {
+        if (patroling)
+        {
+            if (waypoints.Length > 0)
+                PatrolingSystem();
+            else if (waypoints.Length == 0)
+                patroling = false;
+        }
+
+        if (readyToInteract && Input.GetKeyDown(KeyCode.Space))
+        {
+            TalkingSystem();
+            patroling = false;
+            npcAgent.speed = 0;
+            //npcAgent.isStopped = true;
+            playerInteract = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            switch (npcType)
+            {
+                case npcType.villager:
+                    readyToInteract = true;
+                    break;
+                case npcType.merchant:
+                    readyToInteract = true;
+                    break;
+                case npcType.standardEnemy:
+                    searchPlayer = true;
+                    break;
+                default:
+                    print("Error?");
+                    break;
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            patroling = !patroling;
+            readyToInteract = false;
+            searchPlayer = false;
+            npcAgent.speed = startAgentSpeed;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Vector3 playerDirection = other.transform.position - transform.position;
+            float anglePlayerDirection = Vector3.Angle(playerDirection, transform.forward);
+
+            if (anglePlayerDirection < enemyFieldOfView / 2 && raycastPoint != null && searchPlayer)
+            {
+                RaycastHit hit;
+                Ray ray = new Ray(raycastPoint.position, playerDirection.normalized);
+
+                if(Physics.Raycast(ray, out hit, npcCollider.radius))
+                {
+                    if (hit.collider.CompareTag("Player"))
+                    {                        
+                        Debug.DrawLine(raycastPoint.position, hit.transform.position, Color.green);
+                    }
+                }
+            }
+
+            if (readyToInteract && playerInteract)
+            {
+                print("guardami");
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(playerDirection), Time.deltaTime * rotSpeed);
+            }
+        }
+
+        
+    }
+
+    void PatrolingSystem()
+    {
+        npcAgent.destination = waypoints[currentWaypoint].position;
+
+        if (Vector3.Distance(transform.position, waypoints[currentWaypoint].position) <= 0.5f)
+        {
+            if (timeOnWaypoint <= 0)
+            {
+                if (currentWaypoint < waypoints.Length)
+                    currentWaypoint++;
+                if (currentWaypoint == waypoints.Length)
+                    currentWaypoint = 0;
+                timeOnWaypoint = startTimeOnWaypoint;
+            }
+            else
+            {
+                timeOnWaypoint -= Time.deltaTime;
+            }
+        }        
+    }
+
+    void TalkingSystem()
+    {
+        if(npcType == npcType.villager)
+            print("Hello stranger! I used to be an adventurer like you, then I took an arrow to the knee..");
+        if (npcType == npcType.merchant)
+            print("Hello adventurer! Want to buy something?");
+    }
+}
---
+++ GameraInteractive_Test/Assets/Scripts/Player_Behaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Player_Behaviour : MonoBehaviour
+{
+    private NavMeshAgent myNavMeshAgent;
+
+    public LayerMask terrainLayer;
+    
+    void Awake()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {   
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray mouseHitPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(mouseHitPoint, out hit, 1000, terrainLayer))
+            {
+                myNavMeshAgent.destination = hit.point;
+            }
+        }
+    }
+}
